d421d0466567b234749068ffebe491f66cd1853c
Eric <ericsholm@gmail.com>
2020-05-08 17:45:50 -0700

    adding Tim's folder that he sent me

---
+++ timNodeJs/index.js
@@ -0,0 +1,24 @@
+const express = require("express")()
+const fetch = require("node-fetch")
+
+//sets up route, the :means that it will use whatever comes after as the search term i.e. /house
+express.get("/:lookup", async (req, res) => {
+  //parses the url object and stores the search term in a variable
+  const searchTerm = req.params.lookup
+
+  //hits the api with a template literal, which is a made with the addition of variables
+  const getjisho = await fetch(
+    `http://beta.jisho.org/api/v1/search/words?keyword=${searchTerm}`
+  )
+
+  //parses fetched data
+  const parseJisho = await getjisho.json()
+
+  //sends data back to client as json
+  res.json(parseJisho)
+})
+
+//sets express server to listen at port 8000
+express.listen(8000, (err) =>
+  console.log(err || "Node.js/Express Server Running")
+)
