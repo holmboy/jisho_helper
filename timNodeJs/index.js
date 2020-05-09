56626c5ff506e269471270c8821677b96e1efd3b
Eric <ericsholm@gmail.com>
2020-05-08 19:04:05 -0700

    put CORS in

--- timNodeJs/index.js
+++ timNodeJs/index.js
@@ -1,10 +1,14 @@
 const express = require("express")()
 const fetch = require("node-fetch")
+const cors = require("cors")
+
+express.use(cors())
 
 //sets up route, the :means that it will use whatever comes after as the search term i.e. /house
 express.get("/:lookup", async (req, res) => {
   //parses the url object and stores the search term in a variable
-  const searchTerm = req.params.lookup
+  const searchTerm = encodeURIComponent(req.params.lookup) 
+  console.log(searchTerm)
 
   //hits the api with a template literal, which is a made with the addition of variables
   const getjisho = await fetch(
@@ -17,6 +21,8 @@ express.get("/:lookup", async (req, res) => {
   //sends data back to client as json
   res.json(parseJisho)
 })
+
+//res.json(parseJisho.data[0].japanese)
 
 //sets express server to listen at port 8000
 express.listen(8000, (err) =>
